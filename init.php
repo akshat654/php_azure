f24b66669aa340e8f5f09c1091b66b4758a33423
Prashant Khandelwal <f2016930@pilani.bits-pilani.ac.in>
2018-10-25 23:58:47 +0530

    8

--- init.php
+++ init.php
@@ -3,6 +3,12 @@ try {
 	//connecting to server
      $conn = new PDO("sqlsrv:server = tcp:quakesafeserver.database.windows.net,1433; Database = quakesafeDB", "quakesafeuser", "P@ssw0rd");
      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+     $stmt = "SELECT lat1 from footprint";
+     $result = $conn->query($stmt);
+     while($row = $result->fetch()){
+     	echo $row['lat1'];
+     }
+
     }
 catch(PDOException $e)
     {
