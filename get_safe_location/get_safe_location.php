b07397817c20fff8cb551c4dcdcd5212694474cb
Prashant Khandelwal <f2016930@pilani.bits-pilani.ac.in>
2018-10-26 00:05:57 +0530

    8

--- get_safe_location/get_safe_location.php
+++ get_safe_location/get_safe_location.php
@@ -35,36 +35,41 @@ $message = "";
 $response = array();
 
 try{ 
-    $sql = "SELECT lat1,lat2 FROM footprint";  
-    $res = $conn->query($sql); 
-    if($res->rowCount() > 0){ 
-         while($row = $res->fetch()){ 
-            // $lat_ = ($row['lat1']+$row['lat2']+$row['lat3']+$row['lat4'])/4;
-            // $lon_ = ($row['lon1']+$row['lon2']+$row['lon3']+$row['lon4'])/4;
+  $stmt = "SELECT lat1 from footprint";
+     $result = $conn->query($stmt);
+     while($row = $result->fetch()){
+      echo $row['lat1'];
+     }
+    // $sql = "SELECT lat1,lat2 FROM footprint";  
+    // $res = $conn->query($sql); 
+    // if($res->rowCount() > 0){ 
+    //      while($row = $res->fetch()){ 
+    //         // $lat_ = ($row['lat1']+$row['lat2']+$row['lat3']+$row['lat4'])/4;
+    //         // $lon_ = ($row['lon1']+$row['lon2']+$row['lon3']+$row['lon4'])/4;
 
-           // if(distance($lat,$lon,$lat_,$lon_,"K")<=$DISTANCE){
-                array_push($response,array(
-                    "lat1"=>$row['lat1']));
-                    // "lat2"=>$row['lat2'],
-                    // "lat3"=>$row['lat3'],
-                    // "lat4"=>$row['lat4'],
-                    // "lon1"=>$row['lon1'],
-                    // "lon2"=>$row['lon2'],
-                    // "lon3"=>$row['lon3'],
-                    // "lon4"=>$row['lon4'],
-                    // "height"=>$row['height'],
-                    // "radius"=>$row['height']/6));
-           // }
-        }
-        unset($res);
-        $error = False; 
-    } else{ 
-        $message = "No are found"; 
-    } 
+    //        // if(distance($lat,$lon,$lat_,$lon_,"K")<=$DISTANCE){
+    //             array_push($response,array(
+    //                 "lat1"=>$row['lat1']));
+    //                 // "lat2"=>$row['lat2'],
+    //                 // "lat3"=>$row['lat3'],
+    //                 // "lat4"=>$row['lat4'],
+    //                 // "lon1"=>$row['lon1'],
+    //                 // "lon2"=>$row['lon2'],
+    //                 // "lon3"=>$row['lon3'],
+    //                 // "lon4"=>$row['lon4'],
+    //                 // "height"=>$row['height'],
+    //                 // "radius"=>$row['height']/6));
+    //        // }
+    //     }
+    //     unset($res);
+    //     $error = False; 
+    // } else{ 
+    //     $message = "No are found"; 
+    // } 
 
-    echo json_encode(array("error"=>$error,
-    "message"=>$message,
-    "response"=>$response));
+    // echo json_encode(array("error"=>$error,
+    // "message"=>$message,
+    // "response"=>$response));
 
 } catch(PDOException $e){ 
     die("ERROR: Could not able to execute $sql. "  
