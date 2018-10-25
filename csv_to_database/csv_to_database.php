55d0b424402f17a35ae3d7445e00504a1d848b38
Prashant Khandelwal <f2016930@pilani.bits-pilani.ac.in>
2018-10-25 15:09:45 +0530

    second

--- csv_to_database/csv_to_database.php
+++ csv_to_database/csv_to_database.php
@@ -1,16 +1,19 @@
 <?php
-require('init.php');
+require('../init.php');
 try {
-    $fileName = "data_.csv";
+    $fileName = "../data_.csv";
     $file = fopen($fileName, "r"); 
         while (($column = fgetcsv($file, 10000, ",")) !== FALSE) {
             $sqlInsert = "INSERT into footprint (lat1,lon1,lat2,lon2,lat3,lon3,lat4,lon4,height)
                    values ('" . $column[0] . "','" . $column[1] . "','" . $column[2] . "','" . $column[3] . "','" . $column[4] . "','" . $column[5] . "','" . $column[6] . "','" . $column[7] . "','" . $column[8] . "')";
             $conn->exec($sqlInsert);
         }
+        echo json_encode(array('error' => FALSE,
+        'message'=>'Data Imported Successfully!'));
     }
 catch(PDOException $e)
     {
-    echo "Connection failed: " . $e->getMessage();
+        echo json_encode(array('error' => FALSE,
+        'message'=>"Connection failed: " + $e->getMessage()));
     }
 ?>
--- get_safe_location/get_safe_location.php
+++ get_safe_location/get_safe_location.php
@@ -1,5 +1,5 @@
 <?php
-require('init.php');
+require('../init.php');
 
 // if(isset($_POST['lat']){
 
